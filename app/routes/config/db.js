36c2702dd7ced330bba4d896f2e4885e1478bb8b
Kahaan <kahaan.r.patel@gmail.com>
2019-02-20 10:26:18 -0800

    setup db

---
+++ app/routes/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url:
+    "mongodb://guest:twosixnine@notable-shard-00-00-ildlo.mongodb.net:27017,notable-shard-00-01-ildlo.mongodb.net:27017,notable-shard-00-02-ildlo.mongodb.net:27017/test?ssl=true&replicaSet=notable-shard-0&authSource=admin&retryWrites=true"
+};
